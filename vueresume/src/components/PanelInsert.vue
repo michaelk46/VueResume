b019bc416f2e70bb99f4eadebdd6737991d41036
michaelk46@gmail.com <54075389+MikeKinder-Earnin@users.noreply.github.com>
2019-10-05 15:18:41 -0400

    Modifications

---
+++ vueresume/src/components/PanelInsert.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="panelField col-10">
+        PanelField
+    </div>
+</template>
+
+<style scoped>
+  .panelField {    
+    background-color: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+  }
+</style>
---
+++ vueresume/src/components/SidePanel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="side">
+    <PanelInsert/>
+  </div>
+</template>
+
+<script>
+  import PanelInsert from './PanelInsert.vue'
+
+  export default {
+    name: 'SidePanel',
+    components: {
+      PanelInsert
+    },
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
